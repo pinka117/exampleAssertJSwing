d4d02ea57b8fe5a19ba1d5874de2ffe35061b6e6
linda <linda.paolieri.27@gmail.com>
2017-12-18 20:25:31 +0100

    add employee model

---
+++ src/main/java/com/attsw/example/assertjswing/model/Employee.java
@@ -0,0 +1,33 @@
+package com.attsw.example.assertjswing.model;
+
+public class Employee {
+	private String id;
+	private String name;
+	private String mail;
+
+	public Employee(String id, String name, String mail) {
+		this.id = id;
+		this.name = name;
+		this.mail = mail;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+}
---
+++ src/test/java/com/attsw/example/assertjswing/model/EmployeeTest.java
@@ -0,0 +1,45 @@
+package com.attsw.example.assertjswing.model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployeeTest {
+	private Employee e;
+
+	@Before
+	public void setUp() {
+		e = new Employee("1", "mario", "mario@gmail.com");
+	}
+
+	@Test
+	public void idTest() {
+		assertEquals("1", e.getId());
+
+	}
+
+	@Test
+	public void nameTest() {
+		assertEquals("mario", e.getName());
+
+	}
+
+	@Test
+	public void mailTest() {
+		assertEquals("mario@gmail.com", e.getMail());
+
+	}
+	@Test
+	public void mailChangeTest() {
+		e.setMail("mail");
+		assertEquals("mail", e.getMail());
+
+	}
+	@Test
+	public void nameChangeTest() {
+		e.setName("name");
+		assertEquals("name", e.getName());
+	}
+	
+
+}
